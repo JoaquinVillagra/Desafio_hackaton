24c1e19294354e55c786af45e7bb0532f163237e
iibanez <ignacio.ibanez@usach.cl>
2017-10-15 10:54:36 -0300

    ultima

--- 2_f.py
+++ 2_f.py
@@ -3,18 +3,19 @@ import numpy as np
 import pandas
 from sklearn import cross_validation
 
+#son leidos los archivos
 data1 = pandas.read_csv('A.csv')
 data2 = pandas.read_csv('B.csv')
 data3 = pandas.read_csv('C.csv')
 data4 = pandas.read_csv('D.csv')
 
+#son unidos
 datos = pandas.concat([data1, data2, data3, data4])
 
+#se mesclan
 datos = datos.sample(frac = 1)
 datos = datos.sample(frac = 1)
 datos = datos.sample(frac = 1)
-
-#print datos
 
 #seleccionamos las caracteristicas
 selectOpt = ["hackathon.tramo_salud_publico","hackathon.edad","hackathon.estrato","hackathon.ind_morosidad1","hackathon.ind_morosidad2","hackathon.cant_personas_fam","hackathon.cant_hijos_fam","hackathon.tot_mont","hackathon.cant_autos"]
@@ -23,13 +24,13 @@ datos = datos.loc[:,selectOpt]
 
 datos = datos.reset_index(drop=True)
 
-#datos.loc[datos["hackathon.status_salud_publica"] == "S", "hackathon.status_salud_publica"]= 1
-#datos.loc[datos["hackathon.status_salud_publica"] == "N", "hackathon.status_salud_publica"]= 0
+#se cambia el valor de las clases a numericos
 datos.loc[datos["hackathon.tramo_salud_publico"] == "A", "hackathon.tramo_salud_publico"]= 0
 datos.loc[datos["hackathon.tramo_salud_publico"] == "B", "hackathon.tramo_salud_publico"]= 1
-datos.loc[datos["hackathon.tramo_salud_publico"] == "C", "hackathon.tramo_salud_publico"]= 3
-datos.loc[datos["hackathon.tramo_salud_publico"] == "D", "hackathon.tramo_salud_publico"]= 4
+datos.loc[datos["hackathon.tramo_salud_publico"] == "C", "hackathon.tramo_salud_publico"]= 2
+datos.loc[datos["hackathon.tramo_salud_publico"] == "D", "hackathon.tramo_salud_publico"]= 3
 
+#se puede ver la clasificacion
 datos.loc[datos["hackathon.estrato"] == "SIN CLASIFICACION", "hackathon.estrato"]= 0
 datos.loc[datos["hackathon.estrato"] == "ABC1", "hackathon.estrato"]= 5
 datos.loc[datos["hackathon.estrato"] == "C2", "hackathon.estrato"]= 4
@@ -37,32 +38,56 @@ datos.loc[datos["hackathon.estrato"] == "C3", "hackathon.estrato"]= 3
 datos.loc[datos["hackathon.estrato"] == "D", "hackathon.estrato"]= 2
 datos.loc[datos["hackathon.estrato"] == "E", "hackathon.estrato"]= 1
 
-#data.loc[:, 'hackathon.edad':] = (data.loc[:, 'hackathon.edad':] - data.loc[:, 'hackathon.edad':].mean()) / (data.loc[:, 'hackathon.edad':].std())
+#las columnas son transformadas a enteros
 datos["hackathon.tramo_salud_publico"] = datos["hackathon.tramo_salud_publico"].astype('int')
 datos["hackathon.edad"] = datos["hackathon.edad"].astype('int')
 datos["hackathon.estrato"] = datos["hackathon.estrato"].astype('int')
-#datos["hackathon.comuna"] = datos["hackathon.comuna"].astype('int')
 datos["hackathon.ind_morosidad1"] = datos["hackathon.ind_morosidad1"].astype('int')
 datos["hackathon.ind_morosidad2"] = datos["hackathon.ind_morosidad2"].astype('int')
 
-print "random forest" 
+print "Inicio del Random Forest"
 
-model = RandomForestClassifier(n_estimators=500)
+model = RandomForestClassifier(n_estimators=700)
+#se realiza validacion cruzada
+cv = cross_validation.KFold(len(datos), n_folds=8)
 
-np.random.seed(123)
-m_train    = np.random.rand(len(datos)) < 0.7
-df_train = datos.iloc[m_train,]
 
-df_train = df_train.reset_index(drop=True)
+print "Se inicia Random Forest"
 
-df_test  = datos.iloc[~m_train,]
+suma_respuesta = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-df_test = df_test.reset_index(drop=True)
+#se inicia la validacion cruzada
+for traincv, testcv in cv:
+    
+    clf = model.fit(datos.loc[traincv].loc[:, 'hackathon.edad':].values, datos.loc[traincv]['hackathon.tramo_salud_publico'].values)
+    
+    probas = clf.predict(datos.loc[testcv].loc[:, 'hackathon.edad':])
 
-# MODELO
-clf = model.fit(df_train.loc[:, 'hackathon.edad':], df_train["hackathon.tramo_salud_publico"])
-#modelo = LogisticRegression(random_state=1)
-#modelo.fit(df_train[headers], df_train["hackathon.status_salud_publica"])
-probas = clf.predict(df_test.loc[:, 'hackathon.edad':])
+    respuesta = pandas.crosstab(df_test["hackathon.tramo_salud_publico"], probas, rownames=['Actual'], colnames=['Predicted'])
+    
+    suma_respuesta[0]+=respuesta[0][0]
+    suma_respuesta[1]+=respuesta[1][0]
+    suma_respuesta[2]+=respuesta[2][0]
+    suma_respuesta[3]+=respuesta[3][0]
+    suma_respuesta[4]+=respuesta[0][1]
+    suma_respuesta[5]+=respuesta[1][1]
+    suma_respuesta[6]+=respuesta[2][1]
+    suma_respuesta[7]+=respuesta[3][1]
+    suma_respuesta[8]+=respuesta[0][2]
+    suma_respuesta[9]+=respuesta[1][2]
+    suma_respuesta[10]+=respuesta[2][2]
+    suma_respuesta[11]+=respuesta[3][2]
+    suma_respuesta[12]+=respuesta[0][3]
+    suma_respuesta[13]+=respuesta[1][3]
+    suma_respuesta[14]+=respuesta[2][3]
+    suma_respuesta[15]+=respuesta[3][3]
+    
+    print "iteracion: %i"%(it)
+    it+=1
 
-print pandas.crosstab(df_test["hackathon.tramo_salud_publico"], probas, rownames=['Actual'], colnames=['Predicted'])
+#son mostrados los resultados
+print "Exactitud: ",(suma_respuesta[0]+suma_respuesta[5]+suma_respuesta[10]+suma_respuesta[15])/float(len(datos))
+print "%i | %i | %i | %i"%(suma_respuesta[0],suma_respuesta[1],suma_respuesta[2],suma_respuesta[3])
+print "%i | %i | %i | %i"%(suma_respuesta[4],suma_respuesta[5],suma_respuesta[6],suma_respuesta[7])
+print "%i | %i | %i | %i"%(suma_respuesta[8],suma_respuesta[9],suma_respuesta[10],suma_respuesta[11])
+print "%i | %i | %i | %i"%(suma_respuesta[12],suma_respuesta[13],suma_respuesta[14],suma_respuesta[15])
